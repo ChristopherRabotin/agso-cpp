66005a26dfe9455b6165d91cddcfea9fd9e8d518
Christopher Rabotin <christopher.rabotin@gmail.com>
2016-11-13 16:20:41 -0700

    Add namespace examples.

---
+++ src/ns1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(){
+  std::cout << "Namespaces rock;" << std::endl;
+  std::cout << "But they quickly become verbose..." << std::endl;
+  return 0;
+}
---
+++ src/ns2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  cout << "Namespaces rock;" << endl;
+  cout << "But they quickly become verbose..." << endl;
+  return 0;
+}
