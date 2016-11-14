b9ceedee63fd048ab82d5ca632b377707d16bac5
Christopher Rabotin <christopher.rabotin@gmail.com>
2016-11-13 17:51:12 -0700

    Namespaces diff commit

--- src/ns2.cpp
+++ src/ns2.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
+using std::cout;
+using std::endl;
+
 int main(){
-  std::cout << "Namespaces rock;" << std::endl;
-  std::cout << "But they quickly become verbose..." << std::endl;
+  cout << "Namespaces rock;" << endl;
+  cout << "But they quickly become verbose..." << endl;
+  cout << "So just specify which functions you want to use." << endl;
   return 0;
 }
