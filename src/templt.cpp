11093290138311ba536cde9b355b79c5f26c90b2
Christopher Rabotin <christopher.rabotin@gmail.com>
2016-11-13 21:59:56 -0700

    Update templt to work =)

--- src/templt.cpp
+++ src/templt.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
 #include "advoop.hpp"
 
-// Given x and y of class "Type", apply the "*" operator which
+// Given x and y of class "Type", apply the "+" operator which
 // *must* return something of class "Type".
 template<class Type>
-Type multiply(Type x, Type y){
-  return x * y;
+Type addition(Type x, Type y){
+  return x + y;
 }
 
 int main(){
   int ii = 10, ij = 2;
-  std::cout << multiply(ii, ij) << std::endl;
+  std::cout << addition(ii, ij) << std::endl;
 
-  char ci = 10, cj = 2;
-  std::cout << multiply(ci, cj) << std::endl;
+  double ci = 10, cj = 2;
+  std::cout << addition(ci, cj) << std::endl;
 
-  Vector2D v1(1, 4), v2(2, 3);
-  std::cout << multiply(v1, v2) << std::endl;
+  Vector2D v1(1, 4);
+  Vector2D v2(2, 3);
+  Vector2D v3 = addition(v1, v2);
+  std::cout << "[" << v3.x << ", " << v3.y << "]"<< std::endl;
 
-  return 0
+  return 0;
 }
